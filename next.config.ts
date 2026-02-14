d83f3f5780904c079fe9f0b847585410854e2b4a
y-as-7 <youssefaskar111@outlook.com>
2026-02-14 19:52:49 +0400

    feat: Add experimental server components external packages configuration and increase API body parser size limit.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb',
+    },
+  },
 };
 
 export default nextConfig;
